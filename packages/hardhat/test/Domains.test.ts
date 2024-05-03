edd797ad0cdf41604c0a1c18a3fdebf4501d1abc
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-05-03 17:16:26 +0900

    テストコードを修正

--- packages/hardhat/test/Domains.test.ts
+++ packages/hardhat/test/Domains.test.ts
@@ -1,7 +1,7 @@
-import {expect} from "chai";
-import {ethers} from "hardhat";
-import {Domains} from "../typechain-types";
-import {HardhatEthersSigner} from "@nomicfoundation/hardhat-ethers/signers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Domains } from "../typechain-types";
 
 describe("Domains", function () {
   // We define a fixture to reuse the same setup in every test.
@@ -50,24 +50,48 @@ describe("Domains", function () {
       expect(currentBalance).to.equal("1234.0");
     });
 
-    it("Withdraw", async function () {
+    it("Check Register × 2 function", async function () {
       const txn = await domains.register("haruki2", {
         value: ethers.parseEther("1234"),
       });
       await txn.wait();
 
-      const domainOwner = await domains.getAddress("haruki2");
+      const txn2 = await domains.connect(account2).register("haruki3", {
+        value: ethers.parseEther("1234"),
+      });
+      await txn2.wait();
+
+      const domainOwner = await domains.getAddress("haruki");
       expect(domainOwner).to.equal(deployerAddress);
 
       const balance = await ethers.provider.getBalance(domains.target);
       const currentBalance = ethers.formatEther(balance);
-      expect(currentBalance).to.equal("2468.0");
+      expect(currentBalance).to.equal("3702.0");
+    });
+
+    it("Withdraw", async function () {
+      const txn = await domains.register("haruki4", {
+        value: ethers.parseEther("1234"),
+      });
+      await txn.wait();
+
+      const domainOwner = await domains.getAddress("haruki4");
+      expect(domainOwner).to.equal(deployerAddress);
+
+      const balance = await ethers.provider.getBalance(domains.target);
+      const currentBalance = ethers.formatEther(balance);
+      expect(currentBalance).to.equal("4936.0");
 
       await domains.withdraw();
       // check balance after withdraw
       const balance2 = await ethers.provider.getBalance(domains.target);
       const currentBalance2 = ethers.formatEther(balance2);
       expect(currentBalance2).to.equal("0.0");
+
+      // get all names
+      const allNames = await domains.getAllNames();
+      expect(allNames.length).to.equal(4);
+      expect(allNames[0]).to.equal("haruki");
     });
   });
 });
