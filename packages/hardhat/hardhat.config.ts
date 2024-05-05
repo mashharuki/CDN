44e7556b635a95719374fd11c22d53bf0357f5ce
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-05-05 17:50:55 +0900

    update

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,25 +1,23 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import {HardhatUserConfig} from "hardhat/config";
-import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-verify";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+import fs from "fs";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
-import fs from "fs";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/config";
 import path from "path";
+import "solidity-coverage";
+dotenv.config();
 
 // If not set, it uses ours Alchemy's default API key.
 // You can get your own at https://dashboard.alchemyapi.io
 const providerApiKey =
   process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey =
-  process.env.DEPLOYER_PRIVATE_KEY ??
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY 
 // If not set, it uses ours Etherscan default API key.
 const etherscanApiKey =
   process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
@@ -39,7 +37,7 @@ if (!SKIP_LOAD) {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
