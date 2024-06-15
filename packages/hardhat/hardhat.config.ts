e3bd2ad7384984b66d3bf92f5b670b96c60dc40a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-06-15 17:48:34 +0900

    chore: Update hardhat.config.ts to fix import formatting and add missing semicolon

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -7,7 +7,7 @@ import fs from "fs";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
 import "hardhat-gas-reporter";
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import path from "path";
 import "solidity-coverage";
 dotenv.config();
@@ -17,7 +17,7 @@ dotenv.config();
 const providerApiKey =
   process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 // If not set, it uses the hardhat account 0 private key.
-const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY 
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
 // If not set, it uses ours Etherscan default API key.
 const etherscanApiKey =
   process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
@@ -64,7 +64,7 @@ const config: HardhatUserConfig = {
     },
     kura: {
       url: "https://rpc-kura.cross.technology/",
-      accounts: [`${deployerPrivateKey}`],
+      accounts: [`0x${deployerPrivateKey}`],
     },
   },
   // configuration for harhdat-verify plugin
