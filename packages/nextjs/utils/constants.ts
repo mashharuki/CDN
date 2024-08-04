32835b7c50c54311d65421f740569838184d1e4f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-08-04 21:26:04 +0900

    chore: Update BASE_API_URL in constants.ts

--- packages/nextjs/utils/constants.ts
+++ packages/nextjs/utils/constants.ts
@@ -1,3 +1,3 @@
 export const RELAYER_ADDRESS = "0xdec702931fBcaD4f4bB7C1C793E8e5Cdcf7c43A1";
 export const RPC_URL = "https://rpc-kura.cross.technology";
-export const BASE_API_URL = "https://p8vx2t2pfd.execute-api.ap-northeast-1.amazonaws.com/prod";
+export const BASE_API_URL = "https://ew8eixw5qa.execute-api.ap-northeast-1.amazonaws.com/prod";
