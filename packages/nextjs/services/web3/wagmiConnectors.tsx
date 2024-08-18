447052f46cb636ad08599fe080507f67e207ca06
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-08-18 12:07:03 +0900

    ```text feat: Add intmax-walletsdk dependency and additional wallets for IntmaxWallet integration
    Add the intmax-walletsdk dependency to the package.json file and include additional wallets for integration with IntmaxWallet. This allows users to connect their IntmaxWallet accounts to the application and access the supported features.

--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -8,10 +8,51 @@ import {
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import { rainbowkitBurnerWallet } from "burner-connector";
+import { intmaxwalletsdk } from "intmax-walletsdk/rainbowkit";
 import * as chains from "viem/chains";
 import scaffoldConfig from "~~/scaffold.config";
 
 const { onlyLocalBurnerWallet, targetNetworks } = scaffoldConfig;
+
+const additionalWallets = [
+  intmaxwalletsdk({
+    wallet: {
+      url: "https://intmaxwallet-sdk-wallet.vercel.app/",
+      name: "IntmaxWalletSDK Demo",
+      iconUrl: "https://intmaxwallet-sdk-wallet.vercel.app/vite.svg",
+    },
+    metadata: {
+      name: "Rainbow-Kit Demo",
+      description: "Rainbow-Kit Demo",
+      icons: ["https://intmaxwallet-sdk-wallet.vercel.app/vite.svg"],
+    },
+  }),
+  intmaxwalletsdk({
+    mode: "iframe",
+    wallet: {
+      url: "https://intmaxwallet-sdk-wallet.vercel.app/",
+      name: "IntmaxWalletSDK Demo",
+      iconUrl: "https://intmaxwallet-sdk-wallet.vercel.app/vite.svg",
+    },
+    metadata: {
+      name: "Rainbow-Kit Demo",
+      description: "Rainbow-Kit Demo",
+      icons: ["https://intmaxwallet-sdk-wallet.vercel.app/vite.svg"],
+    },
+  }),
+  intmaxwalletsdk({
+    wallet: {
+      url: "https://wallet.intmax.io",
+      name: "INTMAX Wallet",
+      iconUrl: "https://wallet.intmax.io/favicon.ico",
+    },
+    metadata: {
+      name: "Rainbow-Kit Demo",
+      description: "Rainbow-Kit Demo",
+      icons: ["https://intmaxwallet-sdk-wallet.vercel.app/vite.svg"],
+    },
+  }),
+];
 
 const wallets = [
   metaMaskWallet,
@@ -33,6 +74,10 @@ export const wagmiConnectors = connectorsForWallets(
     {
       groupName: "Supported Wallets",
       wallets,
+    },
+    {
+      groupName: "IntmaxWallet",
+      wallets: additionalWallets,
     },
   ],
 
